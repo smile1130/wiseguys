226f35cd30544aa7579a25f3360bd8aee4d70088
smile1130 <superskiller1130@gmail.com>
2024-02-09 17:23:45 +0800

    Add bottom border to Navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -22,13 +22,14 @@ const Navbar = () => {
       break;
   }
   return (
-    <div className="flex items-center justify-between bg-bgPrimary px-6 py-4">
+    <div className="flex items-center justify-between bg-bgPrimary px-6 py-4 relative">
       <div className="flex gap-[70px]">
         <Link to="/">
-          <img src="/logo.png" alt="logo" className="" />
+          <img src="/logo.png" alt="logo" />
         </Link>
         <span className="text-[#BCBCBC] text-18 font-semibold">{title}</span>
       </div>
+      <div className="absolute w-[160px] h-[1px] bg-gray-500/70 left-5 bottom-0"></div>
       <button className="bg-[#0057BD] text-white px-4 py-2 rounded-lg">
         Connect Wallet
       </button>
